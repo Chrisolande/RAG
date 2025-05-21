9a85c279ebd27db66f18ee806fc38a5d695fa351
Chris Olande <olandechris@outlook.com>
2025-05-21 14:15:18 +0300

    Update LangChain community/DocumentLoader package
    The text splitter imports have been updated to use the LangChain Community Package (langchain_community.document_loaders) instead of the deprecated version (langchain_core.document_loaders). This change ensures compatibility with the latest version of LangChain.

--- Contextual Chunk Headers/document_processor.py
+++ Contextual Chunk Headers/document_processor.py
@@ -1,7 +1,7 @@
 import os
 from typing import Dict, List, Any, Optional
 
-from langchain_core.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_core.documents import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema.runnable import RunnablePassthrough
